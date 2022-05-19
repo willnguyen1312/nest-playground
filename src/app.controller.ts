ab98d8d6d552b70699134dc41ee1f3c5d511bf37
Nam Nguyen <nam.nguyen.node@gmail.com>
2022-05-19 07:51:26 +0700

    🎉

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Response, StreamableFile } from '@nestjs/common';
 import { AppService } from './app.service';
+import { createReadStream } from 'fs';
+import { join } from 'path';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,15 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/file')
+  getFile(@Response({ passthrough: true }) res): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'sample.pdf'));
+    res.set({
+      'Content-Type': 'application/pdf',
+      'Content-Disposition': 'inline',
+    });
+    return new StreamableFile(file);
   }
 }
